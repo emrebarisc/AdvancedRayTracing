3b036c9fd68a3ecfb9efaca494475ea0e8ce6d5f
emrebarisc <emrebarisc@gmail.com>
2018-06-06 16:11:57 +0300

    Initial commit.

---
+++ Sources/7 - Object Lights, Path Tracing/DirectionalLight.h
@@ -0,0 +1,34 @@
+/*
+ *	Advanced ray-tracer algorithm
+ *	Emre Baris Coskun
+ *	2018
+ */
+
+#ifndef __DIRECTIONALLIGHT_H__
+#define __DIRECTIONALLIGHT_H__
+
+#include "Light.h"
+
+class DirectionalLight : public Light
+{
+public:
+    DirectionalLight() : Light()
+    {
+
+    }
+
+    ~DirectionalLight() override
+    {
+        
+    }
+
+    Vector3 GetIntensityAtPosition(const Vector3& lightPosition, const Vector3& positionAt) const override;
+
+    bool ShadowCheck(const Vector3& lightPosition, const Vector3& positionAt) const override;
+
+    Vector3 direction;
+    Vector3 radiance;
+private:
+};
+
+#endif
---
+++ Sources/7 - Object Lights, Path Tracing/MemoryManager.h
@@ -0,0 +1,20 @@
+/*
+ *	Advanced ray-tracer algorithm
+ *	Emre Baris Coskun
+ *	2018
+ */
+
+#ifndef __MEMORYMANAGER_H__
+#define __MEMORYMANAGER_H__
+
+class MemoryManager
+{
+public:
+    MemoryManager();
+    ~MemoryManager();
+
+private:
+
+};
+
+#endif
